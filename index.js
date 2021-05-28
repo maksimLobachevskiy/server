dfa6fb5e734939b4b84ee5e98473298e8f9c2a3b
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2021-05-28 19:13:56 +0300

    update server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
@@ -8,15 +8,18 @@ const emails = [
   {
     id: 1,
     topic: "Email 1",
+    text: "Email 1 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptate enim saepe doloribus. Nisi est, neque fugiat quo dolor nemo libero rem, qui similique impedit sequi aliquam ea, blanditiis corporis molestiae sapiente unde? Iste minus iusto earum deleniti accusantium commodi totam, atque dicta expedita, reiciendis amet quos nihil consequuntur repellendus.",
   },
   {
     id: 2,
     topic: "Email 2",
     hidden: true,
+    text: "Email 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptate enim saepe doloribus. Nisi est, neque fugiat quo dolor nemo libero rem, qui similique impedit sequi aliquam ea, blanditiis corporis molestiae sapiente unde? Iste minus iusto earum deleniti accusantium commodi totam, atque dicta expedita, reiciendis amet quos nihil consequuntur repellendus.",
   },
   {
     id: 3,
     topic: "Email 3",
+    text: "Email 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem voluptate enim saepe doloribus. Nisi est, neque fugiat quo dolor nemo libero rem, qui similique impedit sequi aliquam ea, blanditiis corporis molestiae sapiente unde? Iste minus iusto earum deleniti accusantium commodi totam, atque dicta expedita, reiciendis amet quos nihil consequuntur repellendus.",
   },
 ];
 
